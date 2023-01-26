5f3e05dd81457637798d4d3ac7a13971f6e8b06e
Nasir <nasir.rubel99@gmail.com>
2023-01-26 21:45:11 +0600

    gh pages setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, splitVendorChunkPlugin } from "vite";
+import react from "@vitejs/plugin-react";
+import { DateTime } from "luxon";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react(), splitVendorChunkPlugin()],
+	build: {
+		rollupOptions: {
+			output: {
+				entryFileNames: `[name].` + DateTime.now().toUnixInteger() + `.js`,
+				chunkFileNames: `[name].` + DateTime.now().toUnixInteger() + `.js`,
+				assetFileNames: `[name].` + DateTime.now().toUnixInteger() + `.[ext]`,
+			},
+		},
+	},
+});
