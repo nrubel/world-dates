29221f5bbca838720b4eddf60c995acd9d8d6090
Nasir <nasir.rubel99@gmail.com>
2023-01-29 12:56:07 +0600

    luxon init

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import type { FC } from "react";
+import { DateTime } from "luxon";
+import { FC, useEffect } from "react";
 import { useState } from "react";
 
 const App: FC = () => {
-	const [count, setCount] = useState(0);
+	const [dateNow, setDateNow] = useState<DateTime>(DateTime.utc());
 
-	return <>App</>;
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setDateNow(DateTime.now());
+		}, 1000);
+		return () => clearInterval(interval);
+	}, []);
+
+	console.log("====================================");
+	console.log(dateNow);
+	console.log("====================================");
+
+	return (
+		<div className={`container mx-auto max-w-md`}>
+			<h1 className="text-2xl mb-5">Your time</h1>
+		</div>
+	);
 };
 
 export default App;
